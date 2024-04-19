78621931b8ea70360b25106cf7d4f63ef1908f69
mohankumarp235 <mohankumarpalanisamy235@gmail.com>
2024-04-19 13:14:29 +0530

    a

---
+++ NetlifyDemodeploy/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
+export const routes: Routes = [{path:'home',title:'Home',component:HomeComponent},
+{path:'about',title:'About',component:AboutComponent},
+{path:'contact',title:'Contact',component:ContactComponent},
+{path:'',redirectTo:'/home',pathMatch:'full'},
+{path:'**',component:PagenotfoundComponent}
+];
